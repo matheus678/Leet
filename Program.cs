178665c47829f21b178ade0d8b19c82b7cb85fbc
matheus678 <60980352+matheus678@users.noreply.github.com>
2020-05-15 20:08:28 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Leet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma frase");
+            string leet = Console.ReadLine();
+            Console.WriteLine( leet .Replace("A", "4") .Replace("a", "4") .Replace("E", "3") .Replace("e", "3") .Replace("I", "1") .Replace("i", "1") .Replace("L", "1") .Replace("l", "1") .Replace("O", "0") .Replace("o", "0") .Replace("T", "7") .Replace("t", "7") .Replace("S", "5") .Replace("s", "5"));
+        }
+    }
+}
